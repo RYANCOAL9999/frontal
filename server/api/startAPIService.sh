54528eb44bcb3d4111200f6cbcc044c2431d66ff
RYANCOAL9999 <lmf242003@gmail.com>
2025-06-25 02:55:40 +0100

    remove s in startAPIService sh

--- server/api/startAPIService.sh
+++ server/api/startAPIService.sh
@@ -2,4 +2,4 @@
 # This script starts the FastAPI server using uvicorn.
 
 # Start the FastAPI server using uvicorn      
-uvicorn main:app --host 127.0.01 --port s8000 --reload
+uvicorn main:app --host 127.0.01 --port 8000 --reload
